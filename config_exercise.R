f90ab13595400fee97a272f45c12c1b868015dce
Cecilia-Sensalari <ceci.sensa93@gmail.com>
2026-02-01 14:08:09 +0100

    Add configuration file for tasks

---
+++ config_exercise.R
@@ -0,0 +1,12 @@
+# User-defined variables for exercise
+#####################################
+
+# Define sample versions to be compared
+version_1 <- "poc_0.13.1"
+version_2 <- "poc_0.14.0"
+
+# Define scheme
+scheme_input <- "GW_03.3"
+
+# Define water_class(es)
+hydr_class_input <- c("HC1", "HC12", "HC2") # Or a single one: "HC2"
